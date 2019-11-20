5b5445c7e7e92535a35302fc34be8ab57d7fa38d
blankhil <nikhilgowda@gmail.com>
2019-11-20 11:14:36 -0800

    Starting A* search

--- search/search/search.py
+++ search/search/search.py
@@ -165,6 +165,7 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
+    #starting aStarSearch
     util.raiseNotDefined()
 
 
